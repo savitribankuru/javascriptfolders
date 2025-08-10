bb59f03ff878c56868894224fd26048a33863915
savitribankuru <savitribankuru23@gmail.com>
2025-08-10 13:22:00 +0530

    Add files via upload

---
+++ traffic.js
@@ -0,0 +1,16 @@
+function redf(){
+    document.getElementById("message").textContent="stop.."
+    document.getElementById("message").style.color="red"
+    document.getElementById("red").style.backgroundColor="red"
+}
+
+function yellowf(){
+    document.getElementById("message").textContent="Ready to go.."
+    document.getElementById("yellow").style.backgroundColor="yellow"
+    document.getElementById("message").style.color="yellow"
+}
+function greenf(){
+    document.getElementById("green").style.backgroundColor="green"
+    document.getElementById("message").textContent="go.."
+    document.getElementById("message").style.color="green"
+}
